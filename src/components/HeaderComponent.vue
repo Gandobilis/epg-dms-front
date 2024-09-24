5ced3e7f3ce74fc9a9ea3fb857b9cd058c692b53
Gandobilis <lashadeveloper@gmail.com>
2024-09-25 01:16:43 +0400

    ფაილის ექსპორტი წაშლა.

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -7,6 +7,9 @@ import Navigation from "./header/Navigation.vue";
   <header class="flex gap-x-5 p-5 items-center justify-between">
     <navigation/>
 
-    <theme-switcher/>
+    <div class="flex items-center gap-x-5">
+      <a href="https://capital-badly-imp.ngrok-free.app/api/v1/connection-fees/download" target="_blank" class="btn btn-success text-white">ექსპორტი</a>
+      <theme-switcher/>
+    </div>
   </header>
 </template>
