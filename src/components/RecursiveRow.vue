37095f79a54389552fc2abcf67a4084ba4d49333
Gandobilis <lashadeveloper@gmail.com>
2024-10-03 21:17:06 +0400

    division logic added.

--- src/components/RecursiveRow.vue
+++ src/components/RecursiveRow.vue
@@ -16,7 +16,7 @@ const emit = defineEmits(['handleEditClick', 'handleDivideClick'])
 
 <template>
   <template v-for="extraction in records" :key="extraction.id">
-    <tr class="hover:bg-gray-300 hover:cursor-pointer">
+    <tr class="cursor-pointer" :class="{'bg-gray-300': extraction.children.length > 0, 'hover:bg-gray-100': extraction.children.length <= 0}">
       <td>
         <div :style="{ marginLeft: `${level * 10 + (level !== 0 && extraction.children.length === 0 ? 20 : 0)}px` }"
              class="flex items-center gap-x-1.5"><img
@@ -43,13 +43,12 @@ const emit = defineEmits(['handleEditClick', 'handleDivideClick'])
       <td v-text="extraction.purpose"/>
       <td v-text="extraction.description"/>
       <td title="შეცვლა">
-        <img src="/src/assets/edit.svg" alt="edit icon" class="cursor-pointer max-w-8"
-             @click="emit('handleEditClick', extraction)"/>
+        <button @click="emit('handleEditClick', extraction)" :class="{'cursor-not-allowed': extraction.children.length > 0}" :disabled="extraction.children.length > 0">
+        <img src="/src/assets/edit.svg" alt="edit icon" class="max-w-8"/></button>
       </td>
       <td title="გაყოფა">
-        <img src="/src/assets/divide.svg" alt="divide icon"
-             @click="emit('handleDivideClick', [extraction.id, extraction.remainder])"
-             class="cursor-pointer max-w-8"/>
+        <button @click="emit('handleDivideClick', [extraction.id, extraction.remainder])" :class="{'cursor-not-allowed': extraction.children.length > 0}" :disabled="extraction.children.length > 0">
+        <img src="/src/assets/divide.svg" alt="divide icon" class="max-w-8"/></button>
       </td>
     </tr>
     <RecursiveRow @handleDivideClick="emit('handleDivideClick', $event)"
