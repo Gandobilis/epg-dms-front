3d8b5950cdc80a04340449712757065ab09f82eb
gandobilis <lashadeveloper@gmail.com>
2024-11-14 00:34:25 +0400

    main.js improved.

--- src/main.js
+++ src/main.js
@@ -5,13 +5,23 @@ import router from "./router";
 import {createPinia} from 'pinia';
 import {useAuthStore} from "./stores/auth.js";
 
-const app = createApp(App);
-const pinia = createPinia();
-app.use(pinia);
+const initializeApp = async () => {
+    const app = createApp(App);
+    const pinia = createPinia();
+    app.use(pinia);
 
-const authStore = useAuthStore();
+    const authStore = useAuthStore();
+    try {
+        // Restore session before mounting the app
+        await authStore.restoreSession();
+    } catch (error) {
+        console.error("Error restoring session:", error);
+    }
 
-authStore.restoreSession().then(() => {
+    // Use router after session is restored
     app.use(router);
     app.mount('#app');
-});
+};
+
+// Initialize the app
+initializeApp();
