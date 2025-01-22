29dd149b18afefce1b20ea93f732b155f4e4eef4
Gandobilis <lashadeveloper@gmail.com>
2025-01-22 17:17:55 +0400

    const filter bug fixed

--- src/stores/filter.js
+++ src/stores/filter.js
@@ -2,7 +2,7 @@ import {defineStore} from 'pinia';
 import {ref} from 'vue';
 
 export const useFilterStore = defineStore('filter', () => {
-    const filter = ref({
+    let filter = ref({
         region: 'აირჩიეთ რეგიონი',
         serviceCenter: 'აირჩიეთ მ/ც',
         withdrawType: 'აირჩიეთ ტიპი',
